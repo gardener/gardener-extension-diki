b20732f045d8b95fb8a5fd823b088ad73127850d
Aleksandar Savchev <aleksandar.savchev@sap.com>
2026-08-05 17:32:45 +0300

    Add cmd for gardener-extension-diki

---
+++ cmd/gardener-extension-diki/main.go
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"os"
+
+	"github.com/gardener/gardener/pkg/logger"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+
+	"github.com/gardener/gardener-extension-diki/cmd/gardener-extension-diki/app"
+)
+
+func main() {
+	logf.SetLogger(logger.MustNewZapLogger(logger.InfoLevel, logger.FormatJSON))
+
+	ctx := signals.SetupSignalHandler()
+	if err := app.NewServiceControllerCommand().ExecuteContext(ctx); err != nil {
+		logf.Log.Error(err, "Error executing the main controller command")
+		os.Exit(1)
+	}
+}
